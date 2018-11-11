d3ddd9c44fde8b5589b3c9a700d7d2941ca3306d
hessman <anthony.domingue@ynov.com>
2018-11-11 12:29:55 +0100

    Beginning of 3d-daemon

---
+++ scripts/3d-daemon/3d-daemon.py
@@ -0,0 +1,353 @@
+#!/usr/bin/env python3
+
+# 3d-daemon.py
+# Daemon backup tool
+# Anthony DOMINGUE
+# 10/11/2018
+
+from scp import SCPClient
+import paramiko
+import argparse
+import tarfile
+import hashlib
+import signal
+import json
+import sys
+import os
+
+config_dir = "./.config"
+hashes_json = config_dir + "/hashes.json"
+config_json = config_dir + "/config.json"
+
+
+def terminate(code: int, message=""):
+    """
+    Properly exit the script with the good verbose.
+    :param code: The exit code or sig.
+    :type code: int
+    :param message: Optional, the message will be print if str.
+    """
+    if code == 0:
+        write_stdout(message)
+        write_stdout("\nExiting...\n")
+        sys.exit(0)
+    else:
+        if isinstance(message, str):
+            sys.stderr.write("Error " + str(code) + " :\n" + message)
+        sys.stderr.write("\nExiting...\n")
+        sys.exit(code)
+
+
+def write_stdout(message: str):
+    """
+    Write message in sys.stdout if not in quiet mode.
+    :param message: Message to sys.stdout.write
+    :type message: str
+    """
+    if not quiet:
+        sys.stdout.write(message)
+
+
+signal.signal(signal.SIGTERM, terminate)
+signal.signal(signal.SIGINT, terminate)
+
+parser = argparse.ArgumentParser(description="A simple daemon backup tool. " +
+                                             "Configuration in " + config_json,
+                                 epilog="./3d-daemon.py -s ./things ./other_things -o ./data -p 22 -a 192.168.56.100 "
+                                        + "-u root -P 123 -D /var/backup -r Or for daemon use ./3d-daemon.py -d",
+                                 prog="3d-daemon")
+
+basic_args = parser.add_argument_group("required arguments")
+ssh_args = parser.add_argument_group("optional ssh arguments")
+
+parser.add_argument("-q", "--quiet",
+                    help="quiet mode, no stdout",
+                    action="store_true")
+parser.add_argument("-d", "--daemon",
+                    help="deamon mode",
+                    action="store_true")
+
+# Basic arguments group
+basic_args.add_argument("-s", "--sources",
+                        nargs="+",
+                        help="path to the directories to backup",
+                        required=True,
+                        type=str)
+basic_args.add_argument("-o", "--output",
+                        help="path where the directories will be backed up",
+                        required=True,
+                        type=str)
+# SSH arguments group
+ssh_args.add_argument("-p", "--port",
+                      help="ssh port for distant server",
+                      type=str)
+ssh_args.add_argument("-a", "--address",
+                      help="address for distant server",
+                      type=str)
+ssh_args.add_argument("-u", "--username",
+                      help="ssh username for distant server",
+                      type=str)
+ssh_args.add_argument("-P", "--password",
+                      help="ssh password for distant server",
+                      type=str)
+ssh_args.add_argument("-D", "--distant_directory",
+                      help="path of the distant backup directory",
+                      type=str)
+ssh_args.add_argument("-r", "--remove_local",
+                      help="remove the local archive if the transfer is successful",
+                      action="store_true")
+
+
+args = parser.parse_args()
+
+daemon_mode = args.daemon
+# TODO : If daemon -> provision variables with config.json
+quiet = args.quiet
+dirs_to_backup = args.sources
+backup_dir = args.output
+
+ssh = {"isWanted": None, "port": None,
+       "username": None, "address": None,
+       "password": None, "client": None,
+       "directory": None, "localRemoveWanted": None}
+
+# SSH arguments are inclusive, they must be used all together
+if args.port is None and args.username is None and args.address is None and \
+        args.password is None and args.distant_directory is None and args.remove_local:
+    ssh["isWanted"] = False
+elif args.port is not None and args.username is not None and args.address is not None and \
+        args.password is not None and args.distant_directory is not None and args.remove_local is not None:
+    ssh["isWanted"] = True
+    ssh["port"] = args.port
+    ssh["username"] = args.username
+    ssh["address"] = args.address
+    ssh["password"] = args.password
+    ssh["directory"] = args.distant_directory
+    ssh["localRemoveWanted"] = args.remove_local
+else:
+    terminate(101, "Missing argument : for ssh use all ssh arguments are required")
+
+
+def to_tar_gz(source: str, output: str):
+    """
+    Compress the source in tar.gz into the output.
+    :param source: Path of the directory to compress.
+    :type source: str
+    :param output: Path of the output of the archive.
+    :type output: str
+    """
+    with tarfile.open(output, "w:gz") as tar:
+        tar.add(source, arcname=os.path.basename(source))
+
+
+def get_directory_hash(directory: str):
+    """
+    Give a SHA256 hash of the whole given directory by hashing recursively and hash the array of hashes.
+    It hash files content, files name and directories name.
+    :param directory: Path of the directory to hash.
+    :type directory: str
+    :return: hex sha256 hash of the directory.
+    """
+    write_stdout("Computing hash for " + directory + "...\n")
+
+    hashes = []
+    for dir_path, dir_names, file_names in os.walk(directory):
+        sha256 = hashlib.sha256()
+        for file_name in file_names:
+            sha256.update(str(dir_names).encode("utf-8"))
+            sha256.update(str(file_name).encode("utf-8"))
+            with open(dir_path + '/' + file_name, "rb") as f:
+                # Process file 4096bytes per 4096bytes so the RAM will not be full
+                for block in iter(lambda: f.read(4096), b''):
+                    sha256.update(block)
+            hashes.append(sha256.hexdigest())
+    sha256 = hashlib.sha256()
+    for h in hashes:
+        sha256.update(h.encode("utf-8"))
+    write_stdout("Done !\n")
+    return sha256.hexdigest()
+
+
+def get_directory_size(directory: str):
+    """
+    Give the size of the given directory.
+    :param directory: Path of the directory to get size.
+    :return: Size of the directory in bytes.
+    """
+    total_size = 0
+    for dirpath, dirnames, filenames in os.walk(directory):
+        for f in filenames:
+            fp = os.path.join(dirpath, f)
+            total_size += os.path.getsize(fp)
+    return total_size
+
+
+def initialisation():
+    """
+    Check if required directories and files exist and create them if needed.
+    """
+    write_stdout("Initialisation...\n")
+
+    for directory in dirs_to_backup:
+        if not os.path.isdir(directory):
+            terminate(100, "No directory " + directory + " to backup !")
+    if not os.path.exists(backup_dir):
+        try:
+            os.makedirs(backup_dir)
+        except Exception as error:
+            terminate(error.args[0], str(error))
+    if not os.path.exists(config_dir):
+        try:
+            os.makedirs(config_dir)
+        except Exception as error:
+            terminate(error.args[0], str(error))
+    if not os.path.exists(hashes_json):
+        try:
+            with open(hashes_json, 'w') as f:
+                f.write("{}")
+        except Exception as error:
+            terminate(error.args[0], str(error))
+    # Check if the hashes_json file is a valid json, if not repair it
+    if os.path.exists(hashes_json):
+        try:
+            with open(hashes_json, 'r') as f:
+                json.load(f)
+        except Exception as error:
+            sys.stderr.write("Error " + str(error.args[0]) +
+                             "\nInvalid " + hashes_json + " file...\nTry repairing it...\n")
+            try:
+                with open(hashes_json, 'w') as f:
+                    f.write("{}")
+            except Exception as error:
+                terminate(error.args[0], str(error))
+    write_stdout("Done !\n")
+
+
+def local_backup(directory: str, new_hash: str):
+    """
+    Backup locally to the backup_dir.
+    :param directory: Path of the directory to backup.
+    :type directory: str
+    :param new_hash: The hash of the directory to backup.
+    :type new_hash: str
+    :return archive: Dict with archive information.
+    """
+    output_filename = os.path.basename(directory)
+    output_path = backup_dir + '/' + output_filename
+    archive = {"path": output_path, "filename": output_filename}
+    write_stdout("Local backup for " + directory + " in progress...\n")
+    try:
+        to_tar_gz(directory, output_path)
+        write_stdout("Done !\n")
+        update_json(directory, new_hash)
+        return archive
+    except Exception as error:
+        terminate(error.args[0], str(error))
+
+
+def transfer_backup(archive: dict):
+    """
+    Transfer an archive to a distant server by SCP.
+    :param archive: Dictionary with information about the archive to transfer.
+    :type archive: dict
+    """
+    write_stdout("SCP transfer for '" + archive["filename"] + "' archive to " + ssh["address"]
+                 + ":" + ssh["port"] + " in '" + ssh["directory"] + "' in progress...\n")
+    try:
+        ssh["client"] = paramiko.SSHClient()
+        ssh["client"].load_system_host_keys()
+        # AutoAddPolicy for known host to avoid error due to unknown host
+        ssh["client"].set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        ssh["client"].connect("192.168.56.100",
+                              port=ssh["port"],
+                              username=ssh["username"],
+                              password=ssh["password"])
+
+        # I prefer SCP over SFTP because it is more appropriate for just transferring a file
+        with SCPClient(ssh["client"].get_transport()) as scp:
+            scp.put(archive["path"], ssh["directory"] + '/' + archive["filename"])
+        write_stdout("Transfer successful !\n")
+
+        if ssh["localRemoveWanted"]:
+            write_stdout("Removing local copy for '" + archive["filename"] + "'...\n")
+            os.remove(archive["path"])
+            write_stdout("Done !\n")
+
+    except Exception as error:
+        terminate(error.args[0], str(error))
+
+
+def update_json(directory: str, new_hash: str):
+    """
+    Update the hashes_json with directory as key and new_hash as value.
+    :param directory: Path of the backed up directory.
+    :type directory: str
+    :param new_hash: The hash of the new backed up directory.
+    :type new_hash: str
+    """
+    write_stdout("Updating entries...\n")
+    try:
+        with open(hashes_json, 'r+') as f:
+            data = json.load(f)
+            data[directory] = new_hash
+            # Rewrite the new hashes_json
+            f.seek(0)
+            json.dump(data, f, indent=4)
+            f.truncate()
+        write_stdout("Done !\n")
+    except Exception as error:
+        terminate(error.args[0], str(error))
+
+
+def check_for_changes(directory: str):
+    """
+    Compare hash in hashes_json and the return of get_directory_hash for a given directory.
+    :param directory: The directory to check and eventually backup.
+    :type directory: str
+    :return result: Dictionary that contains hash of the directory to check and bool about the checking.
+    """
+
+    # I use a dictionary as return for more readability
+    result = {"hash": get_directory_hash(directory)}
+
+    with open(hashes_json, 'r') as f:
+        data = json.load(f)
+
+    write_stdout("computed hash for " + directory + " : " + result["hash"] + "\n")
+
+    if directory in data:
+        write_stdout("founded  hash for " + directory + " : " + data[directory] + " in " + hashes_json + "\n")
+        if result["hash"] == data[directory]:
+            write_stdout("Same hashes !\n")
+            result["isPositive"] = False
+            return result
+        else:
+            write_stdout("Different hashes !\n")
+            result["isPositive"] = True
+            return result
+    else:
+        write_stdout("no hash found for " + directory + " in " + hashes_json + "\n")
+        write_stdout("New archive !\n")
+        result["isPositive"] = True
+        return result
+
+
+initialisation()
+
+if not daemon_mode:
+    for dir_to_backup in dirs_to_backup:
+        check_result = check_for_changes(dir_to_backup)
+        if check_result["isPositive"]:
+            if ssh["isWanted"]:
+                archive_info = local_backup(dir_to_backup, check_result["hash"])
+                transfer_backup(archive_info)
+            else:
+                local_backup(dir_to_backup, check_result["hash"])
+    terminate(0, "Work is done !")
+else:
+    write_stdout("comming soon !")
+    # TODO : Infinite loop with sleep inside
+        # TODO : For all dirs
+            # TODO : check size and compare with config.json
+                # TODO : local backup if oversized + rm files
+                # TODO : ssh backup if needed
